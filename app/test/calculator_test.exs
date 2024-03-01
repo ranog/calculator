a02b7f6c75d8ac0c3f8f6519cd7dc62dd9cc5e74
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-03-01 18:55:54 -0300

    apply pattern matching in test refactoring

--- app/test/calculator_test.exs
+++ app/test/calculator_test.exs
@@ -1,23 +1,27 @@
 defmodule App.CalculatorTest do
   use ExUnit.Case
 
-  test "should return 4 when multiply 2 by 2" do
-    assert App.Calculator.multiply(2, 2) == 4
+  setup do
+    {:ok, two: 2, four: 4}
   end
 
-  test "should return 6 when multiply 2 by 3" do
-    assert App.Calculator.multiply(2, 3) == 6
+  test "should return 4 when multiply 2 by 2", %{two: number_two, four: number_four} do
+    assert App.Calculator.multiply(number_two, number_two) == number_four
   end
 
-  test "should return nil when multiply 2 by nil" do
-    assert App.Calculator.multiply(2, nil) == nil
+  test "should return 6 when multiply 2 by 3", %{two: number_two} do
+    assert App.Calculator.multiply(number_two, 3) == 6
   end
 
-  test "should return nil when multiply nil by 2" do
-    assert App.Calculator.multiply(nil, 2) == nil
+  test "should return nil when multiply 2 by nil", %{two: number_two} do
+    assert App.Calculator.multiply(number_two, nil) == nil
   end
 
-  test "shouldn't return 10 when multiply 2 by 4" do
-    refute App.Calculator.multiply(2, 4) == 10, "This is an error!!"
+  test "should return nil when multiply nil by 2", %{two: number_two} do
+    assert App.Calculator.multiply(nil, number_two) == nil
+  end
+
+  test "shouldn't return 10 when multiply 2 by 4", %{two: number_two, four: number_four} do
+    refute App.Calculator.multiply(number_two, number_four) == 10, "This is an error!!"
   end
 end
